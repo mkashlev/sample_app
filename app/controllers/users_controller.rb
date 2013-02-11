637aeda14288366268e535005b6931ea74d573b9
Dmitry Kashlev <dmitry@fantoon.com>
2013-02-10 21:07:14 -0800

    Add suer microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def index
@@ -56,14 +57,7 @@ class UsersController < ApplicationController
   end
 
   private
-    
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-    
+        
     def signed_out_user
       unless !signed_in?
         redirect_to root_url
